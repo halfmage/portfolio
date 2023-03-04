ca22112a068e6ef112afbb67c66a8f7c71019fc1
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2023-03-04 22:50:03 +0100

    general updates

--- .eleventy.js
+++ .eleventy.js
@@ -2,12 +2,9 @@ const htmlmin = require('html-minifier')
 const now = String(Date.now())
 
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addWatchTarget('./_tmp/style.css')
-    eleventyConfig.addPassthroughCopy('work')
+    eleventyConfig.addPassthroughCopy('images')
     eleventyConfig.addPassthroughCopy('fonts')
-    eleventyConfig.addPassthroughCopy({ './_tmp/style.css': './style.css' })
     eleventyConfig.addPassthroughCopy({ './node_modules/alpinejs/dist/cdn.js': './alpine.js' })
-    eleventyConfig.addPassthroughCopy({ './node_modules/medium-zoom/dist/medium-zoom.min.js': './medium-zoom.js' })
     eleventyConfig.addShortcode('version', function () {
         return now
     })
