1bf98a05e1fdbc5c199c7a323f5ef962e74685cc
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2024-07-09 12:26:18 +0200

    major updates

--- .eleventy.js
+++ .eleventy.js
@@ -1,48 +1,40 @@
 const htmlmin = require('html-minifier')
-const Image = require("@11ty/eleventy-img")
+const {eleventyImagePlugin} = require("@11ty/eleventy-img")
+const eleventyWebcPlugin = require("@11ty/eleventy-plugin-webc");
 const now = String(Date.now())
-
-async function imageShortcode(src, alt, sizes, imgClass) {
-    if(alt === undefined) {
-        // You bet we throw an error on missing alt (alt="" works okay)
-        throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
-    }
-
-    let metadata = await Image(src, {
-        widths: [300, 600, 1200],
-        formats: ['webp', 'jpg', 'png'],
-        urlPath: "/images/",
-        outputDir: "./_site/images/"
-    });
-
-    let lowsrc = metadata.png[0];
-    let highsrc = metadata.png[metadata.png.length - 1];
-
-    return `<picture>
-        ${Object.values(metadata).map(imageFormat => {
-        return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`;
-        }).join("\n")}
-        <img
-            src="${lowsrc.url}"
-            width="${highsrc.width}"
-            height="${highsrc.height}"
-            alt="${alt}"
-            class="${imgClass}"
-            loading="lazy"
-            decoding="async">
-        </picture>`;
-}
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addWatchTarget('tailwind.config.js')
     eleventyConfig.addWatchTarget('tailwind.css')
-    eleventyConfig.addPassthroughCopy('images')
-    eleventyConfig.addPassthroughCopy('fonts')
+    eleventyConfig.addPassthroughCopy('favicon.png')
+    eleventyConfig.addPassthroughCopy('avatar2023.png')
     eleventyConfig.addPassthroughCopy({ './node_modules/alpinejs/dist/cdn.js': './alpine.js' })
-    eleventyConfig.addShortcode('version', function () {
-        return now
-    })
-    eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode)
+
+	// WebC
+	eleventyConfig.addPlugin(eleventyWebcPlugin, {
+		components: [
+			// …
+			// Add as a global WebC component
+			"npm:@11ty/eleventy-img/*.webc",
+		],
+	});
+
+	// Image plugin
+	eleventyConfig.addPlugin(eleventyImagePlugin, {
+		// Set global default options
+		formats: ["webp", "jpeg"],
+		urlPath: "/img/",
+
+		// Notably `outputDir` is resolved automatically
+		// to the project output directory
+
+		defaultAttributes: {
+			loading: "lazy",
+			decoding: "async",
+		},
+	});
+
+    // HTML minifier
     eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
         if (
             outputPath &&
